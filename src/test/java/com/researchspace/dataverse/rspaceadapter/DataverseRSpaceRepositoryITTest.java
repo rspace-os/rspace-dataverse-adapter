7291910384ab17f7e786954792afb4c1fe777e47
Nico Ferrante <nicola.ferrante@researchspace.com>
2026-03-17 10:27:50 +0000

    RSDEV-955: Add dataverse adapter tests

--- src/test/java/com/researchspace/dataverse/rspaceadapter/DataverseRSpaceRepositoryITTest.java
+++ src/test/java/com/researchspace/dataverse/rspaceadapter/DataverseRSpaceRepositoryITTest.java
@@ -1,22 +1,33 @@
 package com.researchspace.dataverse.rspaceadapter;
 
 import static com.researchspace.core.util.TransformerUtils.toList;
+import static com.researchspace.dataverse.rspaceadapter.DataverseRSpaceRepository.IGSN_INVENTORY_LINKED_ITEMS;
 import static com.researchspace.dataverse.rspaceadapter.DataverseRSpaceRepository.RAID_METADATA_PROPERTY;
+import static com.researchspace.dataverse.rspaceadapter.DataverseRSpaceRepositoryTest.IGSN_ITEM_1;
+import static com.researchspace.dataverse.rspaceadapter.DataverseRSpaceRepositoryTest.IGSN_ITEM_2;
+import static com.researchspace.dataverse.rspaceadapter.DataverseRSpaceRepositoryTest.RAID_REFERENCE;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import com.researchspace.dataverse.api.v1.DataverseAPI;
+import com.researchspace.repository.spi.ControlledVocabularyTerm;
+import com.researchspace.repository.spi.ExternalId;
+import com.researchspace.repository.spi.IDepositor;
 import com.researchspace.repository.spi.LicenseDefs;
+import com.researchspace.repository.spi.RepositoryConfig;
+import com.researchspace.repository.spi.RepositoryOperationResult;
+import com.researchspace.repository.spi.SubmissionMetadata;
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URI;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -25,95 +36,93 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-import com.researchspace.dataverse.api.v1.DataverseAPI;
-import com.researchspace.repository.spi.ExternalId;
-import com.researchspace.repository.spi.IDepositor;
-import com.researchspace.repository.spi.RepositoryConfig;
-import com.researchspace.repository.spi.RepositoryOperationResult;
-import com.researchspace.repository.spi.SubmissionMetadata;
-import com.researchspace.repository.spi.ControlledVocabularyTerm;
-
 @DataverseSpringTest
 public class DataverseRSpaceRepositoryITTest extends AbstractJUnit4SpringContextTests {
-	final String validSubject = "Law";
-	
-	private @Autowired DataverseAPI dvAPI;
-	private @Autowired DataverseRepoConfigurer configurer;
-	DataverseRSpaceRepository adapter;
-	
-	// defaults from test.properties
-	@Value("${dataverseAlias}")
-	protected String dataverseAlias;
-	@Value("${dataverseServerURL}")
-	protected String serverURL;
-	@Value("${dataverseApiKey}")
-	protected String apiKey;
-	File toDeposit = new File("src/test/resources/anyfile.doc");
 
-	@Before
-	public void setUp() {
-		adapter = new DataverseRSpaceRepository();
-		adapter.setDvAPI(dvAPI);
-		adapter.setConfigurer(configurer);
-	}
+  final String validSubject = "Law";
 
-	@After
-	public void tearDown() {
-	}
+  private @Autowired DataverseAPI dvAPI;
+  private @Autowired DataverseRepoConfigurer configurer;
+  DataverseRSpaceRepository adapter;
 
-	@Ignore("requires passing dataverseApiKey in test.properties")
-	@Test
-	public void testConnection() throws MalformedURLException {
-		RepositoryConfig cfg = createRepoConnectionCfg();
-		adapter.configure(cfg);
-		assertTrue(adapter.testConnection().isSucceeded());
-	}
+  // defaults from test.properties
+  @Value("${dataverseAlias}")
+  protected String dataverseAlias;
+  @Value("${dataverseServerURL}")
+  protected String serverURL;
+  @Value("${dataverseApiKey}")
+  protected String apiKey;
+  File toDeposit = new File("src/test/resources/anyfile.doc");
 
-	private RepositoryConfig createRepoConnectionCfg() throws MalformedURLException {
-		return new RepositoryConfig(new URL(serverURL), apiKey, null, dataverseAlias);
-	}
+  @Before
+  public void setUp() {
+    adapter = new DataverseRSpaceRepository();
+    adapter.setDvAPI(dvAPI);
+    adapter.setConfigurer(configurer);
+  }
 
-	@lombok.Value
-	static class Depositor implements IDepositor {
-		String email, uniqueName;
-		List<ExternalId> externalIds;
-	}
+  @After
+  public void tearDown() {
+  }
 
-	@Ignore("requires passing dataverseApiKey in test.properties")
-	@Test
-	public void testDeposit() throws MalformedURLException, URISyntaxException {
-		RepositoryConfig cfg = createRepoConnectionCfg();
+  @Ignore("requires passing dataverseApiKey in test.properties")
+  @Test
+  public void testConnection() throws MalformedURLException {
+    RepositoryConfig cfg = createRepoConnectionCfg();
+    adapter.configure(cfg);
+    assertTrue(adapter.testConnection().isSucceeded());
+  }
 
-		SubmissionMetadata md = new SubmissionMetadata();
-		IDepositor auth = new Depositor("a@b.com", "any", Collections.emptyList());
-		md.setAuthors(toList(auth));
-		md.setContacts(toList(auth));
-		md.setDescription("desc (from DataverseRSpaceRepositoryITTest)");
-		md.setPublish(false);
-		md.setTitle("Title");
-		md.setOtherProperties(Map.of(RAID_METADATA_PROPERTY, "https://raid.org/10.12345/NICO26"));
+  private RepositoryConfig createRepoConnectionCfg() throws MalformedURLException {
+    return new RepositoryConfig(new URL(serverURL), apiKey, null, dataverseAlias);
+  }
 
-		md.setSubjects(toList(validSubject));
+  @lombok.Value
+  static class Depositor implements IDepositor {
 
-		md.setTerms(toList(ControlledVocabularyTerm.builder().value("foo").vocabulary("bar").uri(new URI("https://www.example.com/foo")).build()));
-		//if Hungarian option is supported in dataverse metadata languages, uncomment line below
-		//md.addProperty("metadataLanguage", "hu");
-		md.setLicense(Optional.of(LicenseDefs.CC_0.getUrl()));
-		md.setLicenseName(Optional.of(LicenseDefs.CC_0.getName()));
-		RepositoryOperationResult result = adapter.submitDeposit(auth, toDeposit, md, cfg);
-		assertTrue(result.isSucceeded());
-	}
+    String email, uniqueName;
+    List<ExternalId> externalIds;
+  }
 
-	@Test
-	public void testGetSubjects() {
-		assertEquals(14, adapter.getConfigurer().getSubjects().size());
-	}
+  @Ignore("requires passing dataverseApiKey in test.properties")
+  @Test
+  public void testDeposit() throws MalformedURLException, URISyntaxException {
+    RepositoryConfig cfg = createRepoConnectionCfg();
 
-	@Test
-	public void testConnectionFailure() throws MalformedURLException {
-		RepositoryConfig cfg = new RepositoryConfig(new URL(serverURL), apiKey, null, "unknownRepo");
-		adapter.configure(cfg);
-		assertFalse(adapter.testConnection().isSucceeded());
-	}
+    SubmissionMetadata md = new SubmissionMetadata();
+    IDepositor auth = new Depositor("a@b.com", "any", Collections.emptyList());
+    md.setAuthors(toList(auth));
+    md.setContacts(toList(auth));
+    md.setDescription("desc (from DataverseRSpaceRepositoryITTest)");
+    md.setPublish(false);
+    md.setTitle("Title");
+    Map<String, String> otherPropertiesMap = new HashMap<>();
+    otherPropertiesMap.put(RAID_METADATA_PROPERTY, RAID_REFERENCE);
+    otherPropertiesMap.put(IGSN_INVENTORY_LINKED_ITEMS, IGSN_ITEM_1 + "," + IGSN_ITEM_2);
+    md.setOtherProperties(otherPropertiesMap);
+
+    md.setSubjects(toList(validSubject));
+
+    md.setTerms(toList(ControlledVocabularyTerm.builder().value("foo").vocabulary("bar")
+        .uri(new URI("https://www.example.com/foo")).build()));
+    //if Hungarian option is supported in dataverse metadata languages, uncomment line below
+    //md.addProperty("metadataLanguage", "hu");
+    md.setLicense(Optional.of(LicenseDefs.CC_0.getUrl()));
+    md.setLicenseName(Optional.of(LicenseDefs.CC_0.getName()));
+    RepositoryOperationResult result = adapter.submitDeposit(auth, toDeposit, md, cfg);
+    assertTrue(result.isSucceeded());
+  }
+
+  @Test
+  public void testGetSubjects() {
+    assertEquals(14, adapter.getConfigurer().getSubjects().size());
+  }
+
+  @Test
+  public void testConnectionFailure() throws MalformedURLException {
+    RepositoryConfig cfg = new RepositoryConfig(new URL(serverURL), apiKey, null, "unknownRepo");
+    adapter.configure(cfg);
+    assertFalse(adapter.testConnection().isSucceeded());
+  }
 
 }
